ceee6672d06dc51bbaafae1d8ccfaf22cf522fb0
dipudas8984 <dasdipu8984@gmail.com>
2021-03-15 19:01:56 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PlayList_Renamer-youtube-data-apiV3
+
+
+
+### This script lets you edit your files name accordingly as in youTube playlist index_numbers.(python3)
+
+#### Instructions Bellow:
+* get the playlist_Id from youtube_playlist adress
+* copy [main.py](https://github.com/Dipudas8984/PlayList_Renamer-youtube-data-apiV3/blob/master/main.py) in your files directory
+* install the requirments [requirements.txt](https://github.com/Dipudas8984/PlayList_Renamer-youtube-data-apiV3/blob/master/requirements.txt) (pip install -r requirements.txt)
+* Run The python file inside the directory
+* paste the playlist_Id 
+* See The log output and decide if u have to edit somthing**
+* * You can do that manually 
+* * or if error enounters , look for if u have entered a valid playlist_Id
---
+++ main.py
@@ -0,0 +1,96 @@
+from googleapiclient.discovery import build
+import sys
+import json, time, os
+
+api_key =  "AIzaSyD9JDXhHNhwBLKCIVmUdjf2Bew8NfJ-UwE"
+youtube = build('youtube', 'v3', developerKey=api_key)
+# req = youtube.search().list(q='code with harry', part='snippet', type='video')
+# res = req.execute()
+# num = sys.argv[1]
+playlist_id = input("Enter The Play list Id : ")
+# ----------------
+nextPage = None
+list = []
+try:
+    while True:
+        res = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=playlist_id,
+        maxResults='50',
+        pageToken=nextPage,
+        ).execute()
+        list += res['items']
+        try:
+            nextPage = res['nextPageToken']
+        except :
+            break
+except :
+    print('\ninvalid playlist id , doesnt match !\n')
+    print('Enter a valid playlist id')
+    exit()
+    
+
+channel_name = res['items'][0]['snippet']['channelTitle']
+print(f"\nChannel name : {channel_name}")
+# list_title = [list['items'][i]['snippet']['title'] for i in range(len(list['items']))]
+
+list_title = [i['snippet']['title'] for i in list]
+# for i in range(len(list_title)):
+#     print(i+1, list_title[i])
+# --------------
+
+title_list = list_title
+# title_list = [res['items'][i]['snippet']['title'] for i in range(len(data['items']))]
+names_list = [i for i in os.listdir() if '.mp4' in i]
+
+title = [i.replace('[','_').replace(']','_').replace('?','_').replace(':','_').replace('|','_').replace('+','_').replace('*','_').replace('🔥', '_fire_') for i in title_list]
+names = [i.replace(" ( 720 X 1280 ).mp4", "") for i in names_list]
+
+
+
+def rename_files(list1,list2):
+    title_count = 0
+    for i in list1:
+        title_count += 1
+        name_count = 0
+        for j in list2:
+            name_count += 1
+            if i in j:
+                #print(f"{title_count} > '{i}'' found on index {name_count}***")
+                #print(f"{title_list[title_count-1]}")
+                #print(f"{title_count} {names_list[name_count-1]}")
+                try:
+                	os.rename(names_list[name_count-1], f"{title_count} {names_list[name_count-1]}")
+                	print(f"{title_count} {names_list[name_count-1]}")
+                except:
+                	continue
+                #print(i)
+               
+                
+# print(title)
+
+# print(names)
+#rename_files(title, names)
+def view_list():
+	for i in range(len(title)):
+		print(i+1, title[i])
+                                   
+while True:
+	command = input(f"\n--> (view/rename/exit)? :")
+	if command == 'view' or command == 'v':
+		view_list()
+	elif command == 'rename' or command == 'r':
+		rename_files(title, names)
+	elif command == 'exit'or command == 'e' or command == 'q':
+		print()
+		print("Exiting!")
+		exit()
+	elif command == 'help' or command == 'h' or command == '':
+		print('''
+-->h/help for help message
+-->v/view for viewing the playlist
+-->r/rename for renaming in current directory
+-->e/exit/q for quit
+        \n''')
+	else:
+		print("\nINVALID input !!\n")
